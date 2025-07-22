688747a0ffe5eb0c8badebbc6376604229619eb9
baharyyilmaz <bahar0606yilmaz@gmail.com>
2025-07-22 22:38:02 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Koleksiyon from './pages/Koleksiyon/Koleksiyon';
+import Tasarimcilar from './pages/Tasarimcilar/Tasarimcilar'; 
 import Golden from './pages/Golden/Golden';
 import Nest from './pages/Nest/Nest';
 import Pearls from './pages/Pearls/Pearls';
 import Ironmite from './pages/Ironmite/Ironmite';
-
 import Footer from './components/Footer';
-import DonmeDolapKarti from './components/DonmeDolapKarti';
 import CookieBanner from './components/CookieBanner';
 import Header from './components/Header';
 import About from './pages/About/About';
-import Donme from './components/Donme';
-
+import Contact from './pages/Contact/Contact';
 import ScrollToTop from './components/ScrollToTop';
-
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import Tasarimcilar from './pages/Tasarimcilar/Tasarimcilar';
 
 const App = () => {
   useEffect(() => {
@@ -43,56 +39,22 @@ const App = () => {
           flexDirection: 'column',
         }}
       >
+        <Header />
         <main style={{ flex: 1 }}>
-          <Header />
-
           <Routes>
-            {/* Anasayfa */}
-            <Route
-              path="/"
-              element={
-                <>
-                  <Home />
-                  <div
-                    id="main-content"
-                    style={{
-                      backgroundColor: '#fcfcfc',
-                      padding: '40px 0',
-                    }}
-                  >
-                    <h1
-                      style={{
-                        textAlign: 'center',
-                        padding: '5px 0',
-                        fontSize: '32px',
-                        color: '#232323',
-                      }}
-                    >
-                      KOLEKSİYONUMUZU KEŞFET
-                    </h1>
-                    <DonmeDolapKarti />
-                  </div>
-                </>
-              }
-            />
-
-       
-           
-
-           
-        <Route path="/hakkimizda" element={<About />} />
-         <Route path="/" element={<Home />} />
-        <Route path="home" element={<Home />} />
-        <Route path="/koleksiyon" element={<Koleksiyon />} />
-        <Route path="/golden" element={<Golden />} />
-       <Route path="/nest" element={<Nest />} />
-        <Route path="/pearls" element={<Pearls />} />
-        <Route path="/ironmite" element={<Ironmite />} />
-        <Route path="/about" element={<About/>}/>
- <Route path='/tasarimcilar' element={<Tasarimcilar/>}/>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/hakkimizda" element={<About />} />
+            <Route path="/tasarimcilar" element={<Tasarimcilar />} />
+            <Route path="/koleksiyon" element={<Koleksiyon />} />
+            <Route path="/golden" element={<Golden />} />
+            <Route path="/nest" element={<Nest />} />
+            <Route path="/pearls" element={<Pearls />} />
+            <Route path="/ironmite" element={<Ironmite />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
-
         <Footer />
         <CookieBanner />
       </div>
